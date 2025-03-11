a78e677a208e3435abe0a5dff14bbb345992af77
Akshita Bharti <akshitabharti5@gmail.com>
2025-03-11 17:13:44 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -1,23 +1,45 @@
 #include<stdio.h>
-struct student{
+
+struct student {
     int rollno;
     char name[100];
     float marks;
 };
-int main()
-{
-    int N, search_roll ,i;
-    scanf("%d",&N);
+
+int main() {
+    int N, search_roll, i;
+    
+   
+    scanf("%d", &N);
+    
+   
     struct student students[N];
-    for (i=0;i<N;i++){
-        if(students[i].rollno == search_roll){
-            printf("roll number :%d\n",students[i].rollno);
-            printtf("Name : %s\n",students[i].name);
-            printf("marks: %.2f\n",students[i].marks);
-        }
-        else{
-            printf("Student not found");
-        }
-        return 0;
+    
+    
+    for (i = 0; i < N; i++) {
+        printf("Enter roll number: ");
+        scanf("%d", &students[i].rollno);
+        printf("Enter name: ");
+        scanf("%s", students[i].name);
+        printf("Enter marks: ");
+        scanf("%f", &students[i].marks);
     }
+    
+
+    printf("Enter roll number to search: ");
+    scanf("%d", &search_roll);
+    
+
+    for (i = 0; i < N; i++) {
+        if (students[i].rollno == search_roll) {
+            printf("Roll number: %d\n", students[i].rollno);
+            printf("Name: %s\n", students[i].name);
+            printf("Marks: %.2f\n", students[i].marks);
+            return 0;  
+        }
+    }
+    
+   
+    printf("Student not found\n");
+    return 0;
 }
